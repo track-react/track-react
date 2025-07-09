f428234e09af6f0ece54b4dc6c50d0be8a16023a
emilymcge <emilymcgettrick@gmail.com>
2025-07-09 12:54:19 -0400

    feat(devTool tab) tab shows up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [      
+      { src: 'manifest.json', dest: '.' },
+      { src: 'devtools.html', dest: '.' },
+      { src: 'devtools.js', dest: '.' },
+      { src: 'panel.html', dest: '.' },
+    ],
+    })
+  ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
