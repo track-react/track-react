d7872cd115c292eddea40cff6a02a98658a13581
emilymcge <emilymcgettrick@gmail.com>
2025-07-09 12:17:12 -0400

    feat(skeleton) post message working

--- vite-plugin/babel-plugins/logFetchCalls.js
+++ vite-plugin/babel-plugins/logFetchCalls.js
@@ -1,2 +1,9 @@
-import babel from 'babel';
+import babel from "babel";
 // import babel from '@vitejs/plugin-babel'
+
+//function logic to log fetch
+
+// export default function logFetchCalls() {
+//   console.log("is this getting run");
+//   return window.postMessage("hi from logFetchCalls");
+// }
