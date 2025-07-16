0972e240011cd3abd0c66229194d79ecb12f4382
emilymcge <emilymcgettrick@gmail.com>
2025-07-16 18:19:40 -0400

    fix(plugin) plugin works for typescript repos now

--- vite-plugin/src/index.ts
+++ vite-plugin/src/index.ts
@@ -1,12 +1,13 @@
-//import { Plugin } from 'vite'; //used to be this, but was getting type errors in vite.config of fetch-test repo so I changed it to below:
-import type { PluginOption } from 'vite';
+import { Plugin } from 'vite';
 // import fs from 'fs';
 import path from 'path';
+//import babel from '@rollup/plugin-babel';
 // import { retrieveFetchData } from '../runtime/retrieveFetchData';
 
 import renameFetch from '../babel-plugins/renameFetch.js';
 
-export function fetchPlugin(): PluginOption {
+export function fetchPlugin(): Plugin {
+  console.log('*****ENTERING PLUGIN******');
   // Returns a Vite-compatible plugin object
   // can be imported to vite.config.ts as reactEventsPlugin()
   return {
@@ -23,27 +24,49 @@ export function fetchPlugin(): PluginOption {
     },
 
     //https://rollupjs.org/plugin-development/#transform
+
     async transform(code, id) {
+      // console.log('[plugin] transforming received:', id);
       // ignore all files that don't end in .js .jsx .ts .tsx
+      //or have already been transformed
       if (
         id.includes('node_modules') ||
         id.includes('retrieveFetchData') ||
+        id.includes('retrieveFetchData.ts') ||
+        id.includes('retrieveFetchData.js') || // add this
         !/\.(jsx?|tsx?)$/.test(id)
       ) {
         return null;
+      }
+      if (code.includes('fetch(')) {
+        console.log(`[plugin] ${id} contains fetch calls, transforming...`);
       }
 
       // Use Babel to transform the code with the renameFetch plugin
       const babel = await import('@babel/core');
       const jsxSyntax = (await import('@babel/plugin-syntax-jsx')).default;
+      const tsSyntax = [
+        (await import('@babel/plugin-syntax-typescript')).default,
+        { isTSX: true },
+      ];
+      console.log(`***[plugin] transforming file: ${id}`);
       const result = await babel.transformAsync(code, {
         filename: id,
-        plugins: [jsxSyntax, renameFetch],
+        plugins: [jsxSyntax, ...[tsSyntax], renameFetch],
         sourceMaps: true,
         configFile: false,
+        parserOpts: {
+          sourceType: 'module',
+          plugins: ['jsx', 'typescript'], //added when wouldnt work for typescript in fetchEvents
+        },
       });
+      // console.log('*this is the result.code', result.code);
       // Return the transformed code and map (if available)
       if (result && result.code) {
+        // console.log(
+        //   '********** code transformed in some way!!!! from index ',
+        //   result.code
+        // );
         return {
           code: result.code,
           map: result.map || null,
